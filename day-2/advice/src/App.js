e7b73a2b40f30e706378a5dfa2561753f96926d1
camlloyd2 <cvl8ec@irginia.edu>
2017-05-26 09:56:22 -0400

    day 3 done, worked on same machine

--- day-2/advice/src/App.js
+++ day-2/advice/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import StickyFooter from 'material-ui/BottomNavigation';
+
 
 var quotes = ['Kick ass today', 
               'Act like you know what you\'re doing',
@@ -8,7 +14,6 @@ var quotes = ['Kick ass today',
               'Make sure there\'s money in the bank',
               'Get that git flow', 
               ]
-
 var _this;
 
 class App extends Component {
@@ -25,8 +30,23 @@ class App extends Component {
         <div className="App-header">
           <h2>Great fucking advice</h2>
         </div>
-            <p className='Quote' onClick = {() => changeState()}> {quotes[this.state.index]}</p>
-       </div>
+
+        <div className="Nav"> 
+          <MuiThemeProvider>
+          <Nav />
+          </MuiThemeProvider>
+        </div>
+        <MuiThemeProvider>
+          <RaisedButton label="Next Quote" onClick={() => changeState()}/>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <RaisedButton label="Random Quote" onClick={() => randomState()}/>
+        </MuiThemeProvider>
+            <button className='Quote' onClick = {() => changeState()}> {quotes[this.state.index]} </button>
+        <MuiThemeProvider>
+          <StickyFooter />
+        </MuiThemeProvider>
+        </div>
     );
   }
 }
@@ -42,5 +62,12 @@ function changeState(){
       index: _this.state.index + 1
     })
   }
+}
+
+function randomState(){
+  var rnd = Math.floor(Math.random()*5);
+  _this.setState({
+    index:rnd
+  });
 }
 export default App;
---
+++ day-2/advice/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar, Tabs, Tab} from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <AppBar title="Great Fucking advice">
+        <Tabs>
+        </Tabs>
+      </AppBar>
+    )
+  }
+}
